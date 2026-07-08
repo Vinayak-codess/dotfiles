38a10a426cc1c85fee4a883538668c5568e57b4f
Vinayak Yadav <vinayak.yadav.codes@gmail.com>
2026-07-08 13:33:14 +0530

    Add autostart script for DWM configuration

---
+++ dwm/autostart.sh
@@ -0,0 +1,6 @@
+
+xrandr --output eDP-1 --scale 0.5x0.5 &
+feh --bg-fill /home/cat/pictures/surfer.jpeg &
+xsetroot -cursor_name left_ptr &
+setxkbmap -option caps:swapescape &
+ ~/.config/dwm/dwmstatus.sh &
